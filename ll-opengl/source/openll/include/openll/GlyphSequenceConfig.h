ef5280a683a7a7a116bcee48ea0cdda392c5a759
anne.gropler <anne.gropler@student.hpi.uni-potsdam.de>
2016-12-14 12:34:35 +0100

    added config class for GlyphSequence. not finished

---
+++ ll-opengl/source/openll/include/openll/GlyphSequenceConfig.h
@@ -0,0 +1,69 @@
+#pragma once
+
+#include <vector>
+#include <functional>
+
+#include <openll/Alignment.h>
+#include <openll/FontFace.h>
+#include <openll/LineAnchor.h>
+
+#include <openll/openll_api.h> //TODO: Are all includes needed?
+
+class GlyphSequenceConfig
+{
+public:
+	explicit GlyphSequenceConfig(gloperate_text::FontFace * font);
+	~GlyphSequenceConfig();
+
+	//TODO register callbacks from every GlyphSequence to notify the sequence when config has changed
+	//how to get the function's origin glyhsequence? Say seq1 and seq2 both register a callback?
+	//or should it be only for GlyphSequence, i.e. we don't register callbacks but objects?
+	//TODO unregister callbacks?
+	void addOnConfigChanged(std::function<void()>);
+
+	bool wordWrap() const;
+	void setWordWrap(bool enable);
+
+	float lineWidth() const;
+	void setLineWidth(float lineWidth);
+
+	gloperate_text::Alignment alignment() const;
+	void setAlignment(gloperate_text::Alignment alignment);
+
+	gloperate_text::LineAnchor lineAnchor() const;
+	void setLineAnchor(const gloperate_text::LineAnchor anchor);
+
+	float fontSize() const;
+	void setFontSize(float fontSize);
+
+	gloperate_text::FontFace * fontFace() const;
+	void setFontFace(gloperate_text::FontFace * fontFace);
+
+	const glm::vec4 & fontColor() const;
+	void setFontColor(glm::vec4 fontColor);
+
+
+protected:
+
+	//maybe define a special callback type?
+	//using callback_t = std::function<void()>;
+	std::vector<std::function<void()>> m_registeredOnConfigChanged;
+
+	bool m_wordWrap;
+	float m_lineWidth;
+
+	gloperate_text::Alignment m_alignment;
+	gloperate_text::LineAnchor m_anchor;
+
+	glm::vec4 m_fontColor;
+	gloperate_text::FontFace * m_fontFace;
+	float m_fontSize;
+
+	glm::vec4 m_margins;
+	float m_ppiScale;
+
+	//TODO: is it useful to set them here? Does not make sense for batch processing...
+	//glm::mat4 m_additionalTransform;
+	//std::u32string m_string;
+};
+
---
+++ ll-opengl/source/openll/source/GlyphSequenceConfig.cpp
@@ -0,0 +1,100 @@
+#include "GlyphSequenceConfig.h"
+
+//default values taken from GlyphSequence.cpp
+GlyphSequenceConfig::GlyphSequenceConfig(gloperate_text::FontFace * font)
+	: m_wordWrap(false)
+	, m_lineWidth(0.f)
+	, m_alignment(gloperate_text::Alignment::LeftAligned)
+	, m_anchor(gloperate_text::LineAnchor::Baseline)
+	, m_fontColor(glm::vec4(0.f, 0.f, 0.f, 1.0))
+	, m_fontSize(12.f)
+	, m_margins({ 0.f,0.f, 0.f, 0.f })
+	, m_ppiScale(1.f)
+{
+	m_fontFace = font;
+}
+
+GlyphSequenceConfig::~GlyphSequenceConfig()
+{
+}
+
+void GlyphSequenceConfig::addOnConfigChanged(std::function<void()> callback)
+{
+	m_registeredOnConfigChanged.push_back(callback);
+}
+
+//TODO unregister callbacks?
+
+bool GlyphSequenceConfig::wordWrap() const
+{
+	return m_wordWrap;
+}
+
+void GlyphSequenceConfig::setWordWrap(bool enable)
+{
+	m_wordWrap = enable;
+}
+
+float GlyphSequenceConfig::lineWidth() const
+{
+	assert(m_fontFace);
+	// since typesetting is done in the font faces font size, the
+	// given linewidth has to be scaled to the font faces font size
+	return glm::max(m_lineWidth * m_fontFace->size() / m_fontSize, 0.f);
+}
+
+void GlyphSequenceConfig::setLineWidth(float lineWidth)
+{
+	m_lineWidth = lineWidth;
+}
+
+
+gloperate_text::Alignment GlyphSequenceConfig::alignment() const
+{
+	return m_alignment;
+}
+
+void GlyphSequenceConfig::setAlignment(const gloperate_text::Alignment alignment)
+{
+	m_alignment = alignment;
+}
+
+gloperate_text::LineAnchor GlyphSequenceConfig::lineAnchor() const
+{
+	return m_anchor;
+}
+
+void GlyphSequenceConfig::setLineAnchor(const gloperate_text::LineAnchor anchor)
+{
+	m_anchor = anchor;
+}
+
+const glm::vec4 & GlyphSequenceConfig::fontColor() const
+{
+	return m_fontColor;
+}
+
+void GlyphSequenceConfig::setFontColor(glm::vec4 fontColor)
+{
+	m_fontColor = fontColor;
+}
+
+gloperate_text::FontFace * GlyphSequenceConfig::fontFace() const
+{
+	return m_fontFace;
+}
+
+void GlyphSequenceConfig::setFontFace(gloperate_text::FontFace * fontFace)
+{
+	m_fontFace = fontFace;
+}
+
+float GlyphSequenceConfig::fontSize() const
+{
+	return m_fontSize;
+}
+
+void GlyphSequenceConfig::setFontSize(float fontSize)
+{
+	m_fontSize = fontSize;
+}
