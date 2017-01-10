d38587b1d75340ef4f17dfdbef82f27f09586132
anne.gropler <anne.gropler@student.hpi.uni-potsdam.de>
2017-01-10 11:34:32 +0100

    renamed and adapted the point-based-example to labeling-at-point

--- ll-opengl/source/examples/labeling-at-point/main.cpp
+++ ll-opengl/source/examples/labeling-at-point/main.cpp
@@ -2,19 +2,15 @@
 #include <iostream>
 #include <algorithm>
 
-#include <cpplocate/cpplocate.h>
-#include <cpplocate/ModuleInfo.h>
-
-#include <glm/gtc/constants.hpp>
 #include <glm/vec2.hpp>
 
 #include <glbinding/gl/gl.h>
-#include <glbinding/gl/extension.h>
-#include <glbinding/gl/bitfield.h>
 #include <glbinding/ContextInfo.h>
 #include <glbinding/Version.h>
 
 #include <GLFW/glfw3.h>
+
+#include <glm/gtc/matrix_transform.hpp>
 
 #include <globjects/globjects.h>
 #include <globjects/logging.h>
@@ -28,6 +24,15 @@
 #include <globjects/VertexAttributeBinding.h>
 #include <globjects/Texture.h>
 
+#include <openll/Alignment.h>
+#include <openll/FontFace.h>
+#include <openll/FontLoader.h>
+#include <openll/GlyphRenderer.h>
+#include <openll/GlyphSequence.h>
+#include <openll/GlyphSequenceConfig.h>
+#include <openll/LineAnchor.h>
+#include <openll/stages/GlyphPreparationStage.h>
+
 #include "datapath.inl"
 
 
@@ -36,119 +41,252 @@ using namespace gl;
 
 namespace
 {
-    auto g_frame = 0u;
-    auto g_size = glm::ivec2{ };
+	auto g_frame = 0u;
+	auto g_size = glm::ivec2{};
+	auto g_size_changed = true;
+
+	std::vector<gloperate_text::GlyphVertexCloud> g_vertexClouds;
+	gloperate_text::FontFace * g_font;
+
+	//different configs we will use in this demo
+	std::vector<gloperate_text::GlyphSequenceConfig> g_configs;
+}
+
+gloperate_text::GlyphVertexCloud prepareGlyphSequences(const glm::vec2 origin, std::string string, gloperate_text::GlyphSequenceConfig config)
+{
+	std::vector<gloperate_text::GlyphSequence> sequences;
+
+	gloperate_text::GlyphSequence sequence;
+	std::u32string unicode_string(string.begin(), string.end());
+	sequence.setString(unicode_string);
+
+	// set wordWrap, lineWidth, alignment, lineAnchor, fontFace, fontSize, fontColor
+	sequence.setFromConfig(config);
+
+	const glm::vec4 margins = config.margins();
+
+	// compute  transform matrix
+	glm::mat4 transform;
+	// translate to lower left in NDC
+	transform = glm::translate(transform, glm::vec3(-1.f, -1.f, 0.f));
+	// scale glyphs to NDC size
+	transform = glm::scale(transform, 2.f / glm::vec3(g_size.x, g_size.y, 1.f));
+	// scale glyphs to pixel size with respect to the displays ppi
+	transform = glm::scale(transform, glm::vec3(config.ppiScale()));
+	// translate to origin in point space - scale origin within
+	// margined extend (i.e., viewport with margined areas removed)
+	const auto marginedExtent = glm::vec2(g_size.x, g_size.y) / config.ppiScale()
+		- glm::vec2(margins[3] + margins[1], margins[2] + margins[0]);
+	transform = glm::translate(transform
+		, glm::vec3((0.5f * origin + 0.5f) * marginedExtent, 0.f) + glm::vec3(margins[3], margins[2], 0.f));
+
+	sequence.setAdditionalTransform(transform);
+	sequences.push_back(sequence);
+
+	return gloperate_text::prepareGlyphs(sequences, true);
+}
+
+gloperate_text::GlyphVertexCloud preparePoint(const glm::vec2 origin, gloperate_text::FontFace * font) {
+	const std::string pointText = R"(.)";
+
+	std::vector<gloperate_text::GlyphSequence> sequences;
+
+	gloperate_text::GlyphSequence sequence;
+	std::u32string unicode_string(pointText.begin(), pointText.end());
+	sequence.setString(unicode_string);
+
+	sequence.setWordWrap(true);
+	sequence.setLineWidth(500.f);
+	sequence.setAlignment(gloperate_text::Alignment::Centered);
+	sequence.setLineAnchor(gloperate_text::LineAnchor::Baseline);
+	sequence.setFontFace(font);
+	sequence.setFontSize(64.f);
+
+	const glm::vec4 margins{ 0.f, 0.f, 0.f, 0.f };
+	const float ppiScale = 1.f;
+
+	// compute  transform matrix
+	glm::mat4 transform;
+	// translate to lower left in NDC
+	transform = glm::translate(transform, glm::vec3(-1.f, -1.f, 0.f));
+	// scale glyphs to NDC size
+	transform = glm::scale(transform, 2.f / glm::vec3(g_size.x, g_size.y, 1.f));
+	// scale glyphs to pixel size with respect to the displays ppi
+	transform = glm::scale(transform, glm::vec3(ppiScale));
+	// translate to origin in point space - scale origin within
+	// margined extend (i.e., viewport with margined areas removed)
+	const auto marginedExtent = glm::vec2(g_size.x, g_size.y) / ppiScale
+		- glm::vec2(margins[3] + margins[1], margins[2] + margins[0]);
+	transform = glm::translate(transform
+		, glm::vec3((0.5f * origin + 0.5f) * marginedExtent, 0.f) + glm::vec3(margins[3], margins[2], 0.f));
+
+	sequence.setAdditionalTransform(transform);
+
+	sequences.push_back(sequence);
+
+	return gloperate_text::prepareGlyphs(sequences, true);
+}
+
+
+void annotatePoint(const glm::vec2 origin, const glm::vec2 offset, std::string string, gloperate_text::GlyphSequenceConfig config)
+{
+	g_vertexClouds.push_back(prepareGlyphSequences(origin + offset, string, config));
+	g_vertexClouds.push_back(preparePoint(origin, config.fontFace()));
 }
 
 
 void initialize()
 {
-    const auto dataPath = common::retrieveDataPath("ll-opengl", "dataPath");
+	const auto dataPath = common::retrieveDataPath("ll-opengl", "dataPath");
 
-    // ToDo
+	gloperate_text::FontLoader loader;
+	g_font = loader.load(dataPath + "/fonts/opensansr36.fnt");
+	
+	//initialize all used configs. There is no need to initialize them and never change them. It's just this example.
+	auto defaultConfig = gloperate_text::GlyphSequenceConfig(g_font);
+	g_configs.push_back(defaultConfig);
+
+	auto configBigRed = gloperate_text::GlyphSequenceConfig(g_font);
+	configBigRed.setFontColor(glm::vec4(1.f, 0.f, 0.f, 1.f));
+	configBigRed.setFontSize(32.f);
+	g_configs.push_back(configBigRed);
+
+	auto configVertical = gloperate_text::GlyphSequenceConfig(g_font);
+	configVertical.setAlignment(gloperate_text::Alignment::Centered);
+	configVertical.setAnchor(gloperate_text::LineAnchor::Center);
+	configVertical.setWordWrap(true);
+	configVertical.setLineWidth(0);
+	g_configs.push_back(configVertical);
+
+	glClearColor(1.f, 1.f, 1.f, 1.f);
 }
 
 void deinitialize()
 {
-    // ToDo
-
-    globjects::detachAllObjects();
+	globjects::detachAllObjects();
 }
 
-void draw()
+void draw(gloperate_text::GlyphRenderer &renderer)
 {
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    
-    // ToDo
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-    glViewport(0, 0, g_size.x, g_size.y);
-    
-    // ToDo
+	if (g_size_changed)
+	{
+		glViewport(0, 0, g_size.x, g_size.y);
+
+
+		g_vertexClouds.clear();
+
+		annotatePoint(glm::vec2(-0.2f, 0.f), glm::vec2(0.f, 0.1f), "annotated with default config", g_configs.at(0));
+		annotatePoint(glm::vec2(0.2f, -0.2f), glm::vec2(0.f, -0.1f), "this one also with default config", g_configs.at(0));
+		annotatePoint(glm::vec2(-0.2f, -0.6f), glm::vec2(0.1, 0.f), "is that point clear? hehe point.", g_configs.at(0));
+
+		annotatePoint(glm::vec2(-0.8f, 0.8f), glm::vec2(0.1, -0.05f), "annotated with big red config", g_configs.at(1));
+		annotatePoint(glm::vec2(-0.7f, 0.4f), glm::vec2(0.1, -0.05f), "still big and red!", g_configs.at(1));
+
+		annotatePoint(glm::vec2(0.8f, 0.8f), glm::vec2(0.05f, 0.f), "one line vertical", g_configs.at(2));
+	}
+	
+	gl::glDepthMask(gl::GL_FALSE);
+	gl::glEnable(gl::GL_CULL_FACE);
+	gl::glEnable(gl::GL_BLEND);
+	gl::glBlendFunc(gl::GL_SRC_ALPHA, gl::GL_ONE_MINUS_SRC_ALPHA);
+
+	for (size_t i = 0; i < g_vertexClouds.size(); i++) {
+		renderer.render(g_vertexClouds[i]);
+	}
+
+	gl::glDepthMask(gl::GL_TRUE);
+	gl::glDisable(gl::GL_CULL_FACE);
+	gl::glDisable(gl::GL_BLEND);
+
+	g_size_changed = false;
 }
 
 
 void error(int errnum, const char * errmsg)
 {
-    globjects::critical() << errnum << ": " << errmsg << std::endl;
+	globjects::critical() << errnum << ": " << errmsg << std::endl;
 }
 
 void framebuffer_size_callback(GLFWwindow * /*window*/, int width, int height)
 {
-    g_size = glm::ivec2{ width, height };
+	g_size = glm::ivec2{ width, height };
 
-    // ToDo
+	g_size_changed = true;
 }
 
 void key_callback(GLFWwindow * window, int key, int /*scancode*/, int action, int /*mods*/)
 {
-    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
-        glfwSetWindowShouldClose(window, 1);
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, 1);
 
-    if (key == GLFW_KEY_F5 && action == GLFW_RELEASE)
-        globjects::File::reloadAll();
+	if (key == GLFW_KEY_F5 && action == GLFW_RELEASE)
+		globjects::File::reloadAll();
 }
-
 
 int main()
 {
 #ifdef SYSTEM_DARWIN
-    globjects::critical() << "macOS does currently not support compute shader (OpenGL 4.3. required).";
-    return 0;
+	globjects::critical() << "macOS does currently not support compute shader (OpenGL 4.3. required).";
+	return 0;
 #endif
 
-    // Initialize GLFW
-    if (!glfwInit())
-        return 1;
+	// Initialize GLFW
+	if (!glfwInit())
+		return 1;
 
-    glfwSetErrorCallback(error);
-    glfwDefaultWindowHints();
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, true);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwSetErrorCallback(error);
+	glfwDefaultWindowHints();
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, true);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    // Create a context and, if valid, make it current
-    GLFWwindow * window = glfwCreateWindow(640, 480, "ll-opengl | labeling-at-point", nullptr, nullptr);
-    if (!window)
-    {
-        globjects::critical() << "Context creation failed. Terminate execution.";
+	// Create a context and, if valid, make it current
+	GLFWwindow * window = glfwCreateWindow(640, 480, "ll-opengl | labeling-at-point", nullptr, nullptr);
 
-        glfwTerminate();
-        return -1;
-    }
+	if (!window)
+	{
+		globjects::critical() << "Context creation failed. Terminate execution.";
 
-    glfwSetKeyCallback(window, key_callback);
-    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+		glfwTerminate();
+		return -1;
+	}
 
-    glfwMakeContextCurrent(window);
+	glfwSetKeyCallback(window, key_callback);
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
-    // Initialize globjects (internally initializes glbinding, and registers the current context)
-    globjects::init();
+	glfwMakeContextCurrent(window);
 
-    std::cout << std::endl
-        << "OpenGL Version:  " << glbinding::ContextInfo::version() << std::endl
-        << "OpenGL Vendor:   " << glbinding::ContextInfo::vendor() << std::endl
-        << "OpenGL Renderer: " << glbinding::ContextInfo::renderer() << std::endl << std::endl;
+	// Initialize globjects (internally initializes glbinding, and registers the current context)
+	globjects::init();
 
-    globjects::DebugMessage::enable();
+	std::cout << std::endl
+		<< "OpenGL Version:  " << glbinding::ContextInfo::version() << std::endl
+		<< "OpenGL Vendor:   " << glbinding::ContextInfo::vendor() << std::endl
+		<< "OpenGL Renderer: " << glbinding::ContextInfo::renderer() << std::endl << std::endl;
 
-    // globjects::info() << "Press F5 to reload compute shader." << std::endl << std::endl;
+	globjects::DebugMessage::enable();
 
+	// globjects::info() << "Press F5 to reload compute shader." << std::endl << std::endl;
 
-    glfwGetFramebufferSize(window, &g_size[0], &g_size[1]);
-    initialize();
+	glfwGetFramebufferSize(window, &g_size[0], &g_size[1]);
+	initialize();
 
-    // Main loop
-    while (!glfwWindowShouldClose(window))
-    {
-        glfwPollEvents();
-        draw();
-        glfwSwapBuffers(window);
-    }
-    deinitialize();
+	gloperate_text::GlyphRenderer renderer;
 
-    // Properly shutdown GLFW
-    glfwTerminate();
+	// Main loop
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwPollEvents();
+		draw(renderer);
+		glfwSwapBuffers(window);
+	}
+	deinitialize();
 
-    return 0;
+	// Properly shutdown GLFW
+	glfwTerminate();
+
+	return 0;
 }
