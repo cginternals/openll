84fc0475276203ff4ea0b039668597de805fde92
Simon Krogmann <simon.krogmann@student.hpi.de>
2017-01-18 17:37:28 +0100

    Add missing SuperSampling.h enum file

---
+++ ll-opengl/source/openll/include/openll/SuperSampling.h
@@ -0,0 +1,22 @@
+
+#pragma once
+
+
+namespace gloperate_text
+{
+
+
+enum class SuperSampling : unsigned char
+{
+    None     = 0u,
+    Grid1x3  = 1u,
+    Grid2x4  = 2u,
+    RGSS2x2  = 3u,
+    Quincunx = 4u,
+    Rooks8   = 5u,
+    Grid3x3  = 6u,
+    Grid4x4  = 7u
+};
+
+
+} // namespace gloperate_text
